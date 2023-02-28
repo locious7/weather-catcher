c75c3c5d792f735da5d61a6ccc71184aa8983642
Scosey <59851207+locious7@users.noreply.github.com>
2023-02-28 06:31:57 +0000

    Update comments

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ api_url = f'http://api.weatherapi.com/v1/current.json?key={my_secret}&q={locatio
 # make a call to the weather api using the reqests module
 response = requests.get(api_url)
 
-# check if the api call is valid or not
+# check if the api call is valid or not and print results
 if response.status_code == 200:
     pprint(response.json()["current"]['temp_f'])
 else:
